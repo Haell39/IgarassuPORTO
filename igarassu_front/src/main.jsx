e18f003dd20df49fa76e9b419e70bd36e51ba7f3
Rael <santosrafaelpro@gmail.com>
2025-05-15 05:21:21 -0300

    Update main.jsx

--- igarassu_front/src/main.jsx
+++ igarassu_front/src/main.jsx
@@ -1,17 +1,17 @@
-// src/main.jsx
+
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { createRoot }       from 'react-dom/client'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
 createInertiaApp({
-  // 1) Diz onde estão as “páginas” (suas Home.jsx, Forms.jsx...)
+  // Diz onde estão as “páginas” 
   resolve: name =>
     resolvePageComponent(
       `./pages/${name}.jsx`,
       import.meta.glob('./pages/**/*.jsx')
     ),
 
-  // 2) Como renderizar o App Inertia
+  // Como renderizar o App Inertia
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
   },
